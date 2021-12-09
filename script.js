601c630d1e058592e703ac7e7c9664c2db823a63
georgemaganzini <hyped@teamliquid.com>
2021-12-09 09:53:46 -0800

    start playerTurn functionality

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// const user;
-
 let gamePhase;
 let gameOver;
 let simonSequence;
@@ -13,7 +11,7 @@ const blueBtn = document.querySelector('.blueBtn');
 const redBtn = document.querySelector('.redBtn');
 const greenBtn = document.querySelector('.greenBtn');
 
-gameBoard.addEventListener('click', handleClick);
+gameBoard.addEventListener('click', handlePlayerClick);
 resetBtn.addEventListener('click', init);
 
 function init() {
@@ -25,39 +23,31 @@ function init() {
 	turnOffAll();
 }
 
-function handleClick(event) {
-	console.log(`${event.target} clicked!`);
-}
-
-function switchPhase() {
-	if (gamePhase) {
-		gamePhase = false;
-	} else gamePhase = true;
-}
-
 init();
 
 // generate sequence function
 
-function increaseByOne(num) {
+function increaseBy(num) {
 	for (let i = 0; i < num; i++) {
 		simonSequence.push(Math.floor(Math.random() * 4));
 	}
 }
 
-simonSequence.forEach(function (el, index) {
-	setTimeout(function () {
-		let tempBtn = document.querySelector(`#b${el}`);
-		tempBtn.classList.toggle('highlight');
-		console.log(el, "on");
-		setTimeout(function () {
-			tempBtn.classList.toggle('highlight');
-			console.log(el, "off")
-		}, 500);
-	}, index * 1000);
-});
+// function increaseByOne() {
+// 	simonSequence.push(Math.floor(Math.random() * 4));
+// }
 
-function onOff() {}
+function lightShow() {
+	simonSequence.forEach(function (el, index) {
+		setTimeout(function () {
+			let tempBtn = document.querySelector(`#b${el}`);
+			tempBtn.classList.toggle('highlight');
+			setTimeout(function () {
+				tempBtn.classList.toggle('highlight');
+			}, 500);
+		}, index * 1000);
+	});
+}
 
 function turnOnAll() {
 	yellowBtn.classList.add('highlight');
@@ -73,13 +63,26 @@ function turnOffAll() {
 	greenBtn.classList.remove('highlight');
 }
 
-// read array and blink buttons
-
-// function highlight(color) {
-// color.classList.toggle('highlight');
-// settimeout 1s -> toggle again
-// }
-
-// determine sequence length, then use Math.random to determine color and .push it to the array, once complete switchPhase
-
 // accept user button presses, immediately change gameOver to true once user's array no longer matches sequence array.
+
+// playerTurn - each click that touches a button -> push into array, each time check if array matches simon array up to that index
+
+function handlePlayerClick(event) {
+	if (event.target.id !== 'gameBoard') {
+		let clickedBtn = document.querySelector(`#${event.target.id}`);
+		clickedBtn.classList.toggle('highlight');
+		setTimeout(function () {
+			clickedBtn.classList.toggle('highlight');
+		}, 500);
+		userSequence.push(`${event.target.id[1]}`);
+		console.log(userSequence);
+		// while
+		if (userSequence != simonSequencei)
+	}
+}
+
+function switchPhase() {
+	if (gamePhase) {
+		gamePhase = false;
+	} else gamePhase = true;
+}
